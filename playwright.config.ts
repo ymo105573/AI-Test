1a3218e5aa270e8970512a26577861bafb18d63f
notasodoo2025 <notasodoo2025@gmail.com>
2025-07-02 12:45:56 -0300

    Playwright config (screenshot)

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,8 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
-
+      //  Screenshot solo si falla
+    screenshot: 'only-on-failure',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
 
