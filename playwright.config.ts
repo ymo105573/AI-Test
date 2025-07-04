988b193acc6aa26d3cb135e3b3c02a4159bd8174
notasodoo2025 <notasodoo2025@gmail.com>
2025-07-03 23:08:25 -0300

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,9 +25,7 @@ export default defineConfig({
   reporter: [
     ['list'],
     ['html', { open: 'never' }],
-   ['json', { outputFile: 'results.json' }],
- 
-  ],
+    ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
